692adade4c6c8c8ddb148c716471994dd5de064c
Aleksandr <kino_mac@mail.ru>
2022-02-24 00:01:49 +0300

    Добавлено решение 3-ей задачи

---
+++ Task3/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 46: Написать программу масштабирования фигуры
+
+void GetCoordinates(string data, double k)
+{
+    int coordinate = 0;
+    int digit = 0;
+    bool isCoordinate = false;
+    for (int i = 0; i < data.Length; i++)
+    {
+        if (Int32.TryParse(data[i].ToString(), out digit))
+        {
+            coordinate = 10*coordinate + digit;
+            isCoordinate = true;   
+        }
+        else
+        {
+            if (isCoordinate)
+                {
+                    Console.Write(coordinate*k);
+                    isCoordinate = false;
+                    coordinate = 0;
+                }
+            Console.Write(data[i]);
+        }
+    }
+}
+
+string data = "(100,0) (2,0) (2,20) (0,20)";
+Console.WriteLine("Исходные координаты: " + data);
+Console.Write("при k = 2 получаем ");
+GetCoordinates(data, 2);
+Console.WriteLine();
+Console.Write("при k = 4 получаем ");
+GetCoordinates(data, 4);
+Console.WriteLine();
+Console.Write("при k = 0.5 получаем ");
+GetCoordinates(data, 0.5);
